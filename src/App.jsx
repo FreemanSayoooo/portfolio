fde25be38d7b7ce0ecaead266263010f5917335e
FreemanSayoooo <freemansayo88@gmail.com>
2024-03-04 15:45:58 -0500

    tests

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className='App'>
       <div className='gradient__bg'>
-        <Route index element={<App />}/>
+        <Route index element = "portfolio/"/>
         <NavBar />
         <Header />
 
