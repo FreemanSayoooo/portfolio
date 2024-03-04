c49dd7b4b46cf1320ef9ec333380a54858f15d62
FreemanSayoooo <freemansayo88@gmail.com>
2024-03-04 15:37:59 -0500

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <div className='App'>
       <div className='gradient__bg'>
+        <Route index element={<Portfolio />}/>
         <NavBar />
         <Header />
 
